598e0566d930e739e766b241e02f3c3cad01fbf9
alishalawani <alishalawani@gmail.com>
2020-10-12 18:46:02 -0500

    first commit"

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	Image,
+	TouchableNativeFeedback,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const handlePress = () => {
+		console.log('Text Pressed');
+	};
+	// console.log(require('./assets/icon.png')) //render local or static images
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text numberOfLines={1} onPress={handlePress}>
+				Hello React Native{' '}
+			</Text>
+			<TouchableNativeFeedback onPress={handlePress}>
+				<View
+					style={{
+						width: 200,
+						height: 70,
+						backgroundColor: 'dodgerblue',
+					}}></View>
+			</TouchableNativeFeedback>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		justifyContent: 'center', //vertically
+		alignItems: 'center', //horizontally
+	},
 });
