552f2e24133896488ede623becc3cb3f9d190c8b
alishalawani <alishalawani@gmail.com>
2020-10-22 01:19:07 -0500

    created two views slash components

---
+++ app/config/colors.js
@@ -0,0 +1,6 @@
+//a single place to define our color pallet 
+export default {
+	black: '#000',
+	primary: '#fc5c65',
+	secondary: '#4ecdc4',
+};
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+function WelcomeScreen(props) {
+	return (
+		<ImageBackground
+			style={styles.background}
+			source={require('../assets/background.jpg')}>
+			<View style={styles.logoContainer}>
+				<Image source={require('../assets/logo.png')} style={styles.logo} />
+				<Text style={styles.text}>Google is your best friend</Text>
+			</View>
+
+			<View style={styles.loginButton} />
+			<View style={styles.registerButton} />
+		</ImageBackground>
+	);
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+	background: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+	loginButton: {
+		width: '100%',
+		height: 70,
+		backgroundColor: 'purple',
+	},
+	registerButton: {
+		width: '100%',
+		height: 70,
+		backgroundColor: '#4ecdc4',
+	},
+	logo: {
+		width: 200,
+		height: 200,
+    },
+    logoContainer:{
+        position: 'absolute',
+        top:70,
+        alignItems:'center',
+    },
+    text:{
+        color:'white',
+        fontSize: 20,
+    }
+});
